5bd4b5a6f5e1fdbb48f2ab4ecc1ca988d7c3ed3f
choongmankim <cm730712@gmail.com>
2022-12-07 19:22:58 +0900

    fix: 헤더 스타일링 수정

--- src/Layout/Header.jsx
+++ src/Layout/Header.jsx
@@ -1,4 +1,5 @@
 import { Fragment } from "react";
+import HeaderCartBtn from "./HeaderCartBtn";
 import headImg from "../assets/head.jpg";
 
 import classes from "./Header.module.css";
@@ -7,8 +8,8 @@ const Header = () => {
   return (
     <Fragment>
       <header className={classes.header}>
-        <h1>Meals</h1>
-        <button>Cart</button>
+        <h1 className={classes.h1}>Meals</h1>
+        <HeaderCartBtn />
       </header>
       <div className={classes["main-image"]}>
         <img src={headImg} alt='main img' />
