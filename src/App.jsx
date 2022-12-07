f57904c624e869818d2ea86ebaf12644669106da
choongmankim <cm730712@gmail.com>
2022-12-07 19:20:55 +0900

    Add: stylde-reset을  App 컴포넌트에 적용

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { Fragment, useState } from "react";
 import Header from "./Layout/Header";
 
+import { Reset } from "styled-reset";
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <Fragment>
+      <Reset />
       <Header />
     </Fragment>
   );
