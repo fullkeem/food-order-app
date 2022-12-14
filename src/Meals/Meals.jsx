6ecbd0d8daa01bedb09a973cb428d0f7b35c4e9e
choongmankim <cm730712@gmail.com>
2022-12-14 20:17:14 +0900

    Add: 메뉴목록 컴포넌트 생성

---
+++ src/Meals/Meals.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+import AvailableMeals from "./AvailableMeals";
+import MealsSummary from "./MealsSummary";
+
+const Meals = () => {
+  return (
+    <section>
+      <MealsSummary />
+      <AvailableMeals />
+    </section>
+  );
+};
+
+export default Meals;
